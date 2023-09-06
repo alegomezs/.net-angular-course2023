e538041954a036fe0741e24954423be85d0917d9
Alejandro Gómez Santillán <ale.gomezs@outlook.com>
2023-09-06 19:52:29 -0300

    New changes

--- GuessGame/assets/js/main.js
+++ GuessGame/assets/js/main.js
@@ -1,50 +1,59 @@
-        let result;
-        let score = 10;
-        let highScore = 0;
-        let message = document.getElementById('message');
-        let resultText =  document.getElementById('result');
-        
-        document.getElementById('reset').addEventListener('click', resetGame);
-        document.getElementById('send').addEventListener('click', guess);
-        
+document.addEventListener("DOMContentLoaded", function(event) {
+    let result;
+    let score = 10;
+    let highScore = 0;
+    let hint = document.getElementById('hint-text');
+    let resultText =  document.getElementById('result');
+    let send =  document.getElementById('send');
+    let reset =  document.getElementById('reset');      
+
+    send.addEventListener('click', function(event){
+        event.preventDefault()
+      });
+    reset.addEventListener('click', function(event){
+        event.preventDefault()
+    });
+
+    reset.addEventListener('click', resetGame);
+    send.addEventListener('click', guess);
+    
+    generateNumber();
+
+    function generateNumber() {
+        result = Math.floor(Math.random() * 20) + 1;
+    }
+
+    function resetGame() {
+        score = 10;
+        resultText.textContent = '?';
+        document.getElementById('highscore').textContent = highScore;
+        hint.textContent = '';
         generateNumber();
+    }
 
-        function generateNumber() {
-            result = Math.floor(Math.random() * 100) + 1;
-        }
+    function guess() {
+        const guessNumber = parseInt(document.getElementById('guess').value);
 
-        function resetGame() {
-            score = 10;
-            resultText.innerText('?');
+        if (guessNumber === result) {
+            hint.textContent = '¡Felicidades! Has adivinado el número correctamente.';
+            resultText.textContent = result;
+            score += 1;
+            if (score > highScore) {
+                highScore = score;
+            }
+            document.getElementById('score').textContent = highScore;
             document.getElementById('highscore').textContent = highScore;
-            message.textContent = '';
-            generateNumber();
-        }
-
-        function guess() {
-            const guessNumber = parseInt(document.getElementById('guess').value);
-
-            if (guessNumber === result) {
-                message.textContent = '¡Felicidades! Has adivinado el número correctamente.';
-                score += 1;
-                if (score > highScore) {
-                    highScore = score;
-                }
-                resultText.innerText(result);                
+            resultText.textContent = result;                
+        } else {
+            score -= 1;
+            if (score === 0) {
+                hint.textContent = `Has agotado tus intentos. El número correcto era ${result}.`;
+                resetGame();
+            } else if (guessNumber < result) {
+                hint.textContent = 'Muy bajo!';
             } else {
-                score -= 1;
-                if (score === 0) {
-                    message.textContent = `Has agotado tus intentos. El número correcto era ${result}.`;
-                    resetGame();
-                } else if (guessNumber < result) {
-                    message.textContent = 'El número es mayor. Inténtalo de nuevo.';
-                } else {
-                    message.textContent = 'El número es menor. Inténtalo de nuevo.';
-                }
+                hint.textContent = 'Muy Alto!';
             }
         }
-
-      
-        
-        
-    
+    }
+});            
