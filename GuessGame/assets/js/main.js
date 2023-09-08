c0c9a39d3e2cc13cf46e54095237c478309a5d7e
Alejandro Gómez Santillán <ale.gomezs@outlook.com>
2023-09-07 21:59:55 -0300

    Last changes

--- GuessGame/assets/js/main.js
+++ GuessGame/assets/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     let result;
-    let score = 10;
+    let score = 20;
     let highScore = 0;
     let hint = document.getElementById('hint-text');
     let resultText =  document.getElementById('result');
@@ -24,37 +24,74 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
     function resetGame() {
-        score = 0   ;
+        score = 20;
+        document.getElementById('body').classList.remove('winner');  
+        document.getElementById('guess').value =  '';        
         resultText.textContent = '?';
         document.getElementById('highscore').textContent = highScore;
+        document.getElementById('score').textContent = score;
         hint.textContent = '';
         generateNumber();
     }
 
-    function guess() {
+    function guess() {    
         const guessNumber = parseInt(document.getElementById('guess').value);
+        const condition = validation(guessNumber);
 
-        if (guessNumber === result) {
-            hint.textContent = 'Numero correcto!';
-            document.getElementsByTagName('body').classList.add('winner').    
-            resultText.textContent = result;
-            score += 1;
-            if (score > highScore) {
-                highScore = score;
-            }
-            document.getElementById('score').textContent = highScore;
-            document.getElementById('highscore').textContent = highScore;
-            resultText.textContent = result;                
-        } else {
-            score -= 1;
-            if (score === 0) {
-                hint.textContent = `Has agotado tus intentos. El número correcto era ${result}.`;
-                resetGame();
-            } else if (guessNumber < result) {
-                hint.textContent = 'Muy bajo!';
+        if (condition.isValid) {
+            if (score !== 0) {     
+                if (guessNumber === result) {
+                    hint.textContent = 'Numero correcto!';
+                    document.getElementById('body').classList.add('winner');                       
+                    resultText.textContent = result;            
+                    if (score > highScore) {
+                        highScore = score;
+                    }
+                    document.getElementById('score').textContent = highScore;
+                    document.getElementById('highscore').textContent = highScore;
+                    resultText.textContent = result;                
+                } else {            
+                    score -= 1;                
+                    if (score === 0) {
+                        console.log(score);
+                        hint.textContent = `Has agotado tus intentos. El número correcto era ${result}.`;            
+                        document.getElementById('score').textContent = score;
+                        resultText.textContent = result; 
+                    } else if (guessNumber < result) {
+                        hint.textContent = 'Muy bajo!';
+                        document.getElementById('score').textContent = score; 
+                    } else {
+                        hint.textContent = 'Muy Alto!';
+                        document.getElementById('score').textContent = score; 
+                    }
+                }
             } else {
-                hint.textContent = 'Muy Alto!';
-            }
+                hint.textContent = `Chuck Norris lo hubiera hecho menos intentos. Debes reiniciar el juego!`;
+            }                  
         }
+        else {
+            hint.textContent = condition.message;
+        }
+    }
+
+    function validation(generateNumber) {
+        const guessNumber = parseInt(document.getElementById('guess').value);
+        const validation = {
+            isValid : false,
+            message : "",
+        };
+        if (!isNaN(guessNumber)) {            
+            if (guessNumber > 0 && guessNumber <= 20) {
+                validation.isValid = true;
+                return validation;
+            }
+            else {
+                validation.message = 'Debe ingresar un valor entre 1 y 20!';                            
+            }
+        } 
+        else {
+            validation.message = 'Debe ingresar un valor numérico!';
+        }   
+        return validation;
     }
 });            
